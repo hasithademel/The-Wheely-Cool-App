8caa82270b21f28408a6f98cd8751b1b1594eb49
Hasitha De Mel <h.mel@we-are-mea.com>
2021-12-20 20:34:09 +1300

    Initial project commit

---
+++ The Wheely Cool App/The Wheely Cool App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  The Wheely Cool App
+//
+//  Created by Hasitha De Mel on 20/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
